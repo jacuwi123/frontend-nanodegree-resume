f988d5b9225f7407ee9556ea52063b2a2a426b52
James Williams <jacuwi123@gmail.com>
2015-10-22 13:51:11 -0600

    add 4 objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,69 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/*var age = 31;
+var firstName = "James"
+console.log(firstName);*/
+var awesomeThoughts = "I am James and I am awesome!";
+//console.log(awesomeThoughts);
+
+// $("#main").append("James");
+// [string].replace([old], [new]);
+
+/*var email = "jacuwi123@gmail.com";
+var newEmail =
+	email.replace("gmail", "yahoo");
+
+console.log(email);
+console.log(newEmail);*/
+
+/*var funThoughts = awesomeThoughts.replace("awesome", "FUN");
+console.log(awesomeThoughts);
+console.log(funThoughts);
+$("#main").append(funThoughts);*/
+
+//var formattedName = HTMLheaderName.replace("%data%", "James Williams");
+// $("#header").prepend(formattedName);
+// var formattedRole = HTMLheaderRole.replace("%data%", "Front-end-developer");
+// $("#header").prepend(formattedRole);
+ var bio = {
+	"name" : "James",
+	"role" : "Front-end-developer",
+	"contactInfo": {
+		"mobile": "970-773-3746",
+		"email": "jacuwi123@gmail.com",
+		"github": "jacuwi123",
+		"location": "Denver"
+	},
+	"welcomeMessage": "Hello World",
+	"skills": [
+		"Typing", "Reading", "Debugging"
+		],
+	"bioPic": "http://placehold.it/50x50"
+}; 
+var education = {
+	"schools": [
+	{
+		"name": "Western State College",
+		"city": "Gunnison, CO, US",
+		"major": "English"
+	},
+	{
+		"name": "Colorado Mesa University",
+		"city": "Grand Junction, CO, US",
+		"major": "English"
+	}
+
+	],
+}
+ var work = {};
+work.position = "Front-end-developer";
+work.employer = "5 Guys";
+work.years = 0.4;
+work.city = "Denver";
+
+
+
+
+
+
+
+
+
