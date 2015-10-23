9fa0f31bd76a1568bd9a79954d9bc9077ee6fedf
James Williams <jacuwi123@gmail.com>
2015-10-23 08:31:13 -0600

    Validate

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 /*var age = 31;
 var firstName = "James"
 console.log(firstName);*/
-var awesomeThoughts = "I am James and I am awesome!";
+//var awesomeThoughts = "I am James and I am awesome!";
 //console.log(awesomeThoughts);
 
 // $("#main").append("James");
@@ -16,57 +16,102 @@ console.log(newEmail);*/
 
 /*var funThoughts = awesomeThoughts.replace("awesome", "FUN");
 console.log(awesomeThoughts);
-console.log(funThoughts);
-$("#main").append(funThoughts);*/
+/*var age = 31;
+var firstName = "James"
+console.log(firstName);*/
+//var awesomeThoughts = "I am James and I am awesome!";
+//console.log(awesomeThoughts);
 
-/* var formattedName = HTMLheaderName.replace("%data%", "James Williams");
- $("#header").prepend(formattedName);
- var formattedRole = HTMLheaderRole.replace("%data%", "Front-end-developer");
- $("#header").prepend(formattedRole); */
+// $("#main").append("James");
+// [string].replace([old], [new]);
+
+/*var email = "jacuwi123@gmail.com";
+var newEmail =
+	email.replace("gmail", "yahoo");*/
+
+
+//var formattedName = HTMLheaderName.replace("%data%", "James Williams");
+// $("#header").prepend(formattedName);
+// var formattedRole = HTMLheaderRole.replace("%data%", "Front-end-developer");
+// $("#header").prepend(formattedRole);
+
+
+
+
+
+ var formattedName = HTMLheaderName.replace("%data%", "James Williams");
+$("#header").append(formattedName);
+var formattedRole = HTMLheaderRole.replace("%data%", "Front-end-Web-development");
+$("#header").append(formattedRole); 
 
 
 var bio = {
-	"name" : "James",
-	"role" : "Front-end-developer",
+	"name" : "James Williams",
+	"role": "Front-end-Web-development",
 	"contactInfo": {
 		"mobile": "970-773-3746",
 		"email": "jacuwi123@gmail.com",
 		"github": "jacuwi123",
-		"location": "Denver"
-	},
-	"welcomeMessage": "Hello World",
+		"location": "Denver, CO"
+},
+	"welcomeMessage": "Grow with every opportunity provided me while remaining motivated positive and energetic.",
 	"skills": [
-		"Typing", "Reading", "Debugging"
-		],
-	"bioPic": "http://placehold.it/50x50"
-}; 
-var education = {
-	"schools": [
-	{
-		"name": "Western State College",
-		"city": "Gunnison, CO, US",
-		"major": "English"
-	},
-	{
-		"name": "Colorado Mesa University",
-		"city": "Grand Junction, CO, US",
-		"major": "English"
-	}
+		"Data Entry", "Time Management", "Reading", "Inventory Management", "High Volume Production", "Cost Controll", "Basic HTML/CSS", "Microsoft Office" 
+	]
+} 
 
-	],
-}
- var work = {};
-work.position = "Front-end-developer";
-work.employer = "5 Guys";
-work.years = 0.4;
-work.city = "Denver"; 
+ var education = {
+ 	"schools": [
+ 	{
+ 		"name": "Western State College",
+ 		"city": "Gunnison, CO",
+ 		"major": "English"
+ 	},
+ 	{
+ 		"name": "Colorado Mesa University",
+ 		"city": "Grand Junction, CO",
+ 		"major": "English"
+ 	}
+],
+ 	"onlineCourses": [
+		 {
+ 		"title": "Intro to HTML and CSS",
+ 		"school": "Udacity",
+ 		"dates": "August 2015",
+ 		"url": "https://www.udacity.com"
+ 		},
+ 		{
+ 		"title": "JavaScript Basics",
+ 		"school": "Udacity",
+ 		"dates": "September 2015",
+ 		"url": "https://www.udacity.com"
+ 		}
+ 	]
+ }
 
+ var work = {
+ 	"jobs": [
+ 		{
+ 			"employer": "Buff Bros Inc.",
+ 			"title": [ "Prep/Line Cook", "Assistant Kitchen Manager", "Kitchen Manager" ],
+ 			"dates": "April 2005 through January 2007",
+ 			"description": "Maintained constant food quality and presentation, Expertly estimated staffing needs and adjusted hourly schedules in accordance with demand patterns, and budget. Provided courteous and informative customer service."
+ 		},
+ 		{
+ 			"employer": "Lil Pub Company",
+ 			"title": "Kitchen Manager",
+ 			"dates": "September 2006 through October 2008",
+ 			"description": "Maintained constant food quality and presentation, Expertly estimated staffing needs and adjusted hourly schedules in accordance with demand patterns, and budget. Provided courteous and informative customer service."
+ 		}
+ 	]
+ }
 
-
-
-
-
-
-
-
-
+ var projects = {
+ 	"projects": [
+ 		{
+ 			"title": "Project One",
+ 			"dates": "August, September-2015",
+ 			"description": "Built a portfolio site using basic HTML and CSS"
+ 		}
+ 	]
+ }
