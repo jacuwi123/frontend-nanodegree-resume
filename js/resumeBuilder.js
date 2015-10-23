1a741e4afd0703085a61cf60bde77d44d713be51
James Williams <jacuwi123@gmail.com>
2015-10-23 11:25:19 -0600

    add work experience

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -150,3 +150,14 @@ if(bio.skills.length > 0) {
 		formattedSkill = HTMLskills.replace("%data%", bio.skills[7]);
 
 }
+
+for (job in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+	$(".work-entry:last").append(formattedEmployerTitle);
+}
+
